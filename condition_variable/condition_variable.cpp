bf2cce01dd03df2fe8dc4521533b083003993045
unknown <xyji@bigvisiontech.com>
2022-08-28 10:55:59 +0800

    重点中的重点：wait函数调用的时候，会阻塞线程，并且接触lck,如果通知接触wait阻塞的话，那么lck会锁定,以前只理解会调用wait的时候会解锁，不知道，解除阻塞的时候，会在此锁住

--- condition_variable/condition_variable.cpp
+++ condition_variable/condition_variable.cpp
@@ -2,10 +2,97 @@
 //
 
 #include <iostream>
+#include <thread>
+#include <condition_variable>
+#include <mutex>
+
+std::condition_variable cv;
+std::mutex mtx;
+bool flag = false;
+void print_int(int i)
+{
+	//这里不能用std::lock_guard 因为这个对象只有在析构的时候会在析构函数中解锁，
+	//而condition_variable的wait函数一旦调用，会解锁，所以只有unique_lock才满足函数执行当中解锁，上锁
+	std::unique_lock<std::mutex> lck(mtx);
+	while (!flag)
+	{
+		// wait会解锁并且阻塞当前线程
+		cv.wait(lck);
+	}
+	std::cout << "thread[" << i << "]:" << std::this_thread::get_id() << std::endl;
+}
+
+void go()
+{
+	std::lock_guard<std::mutex> lck(mtx);
+	flag = true;
+	cv.notify_all();
+	//cv.notify_one();
+}
+
+std::condition_variable produce, consume;
+
+int cargo = 0;     // shared value by producers and consumers
+
+void consumer() {
+	
+	std::unique_lock<std::mutex> lck(mtx);
+	/*std::cout << "thread id:" << std::this_thread::get_id() << std::endl;*/
+	while (cargo == 0) 
+	{
+		std::cout << "cargo test:" << cargo << std::endl;
+		consume.wait(lck);
+	}
+	std::cout << cargo << '\n';
+	//std::this_thread::sleep_for(std::chrono::seconds(10));
+	cargo = 0;
+	//produce.notify_one();
+}
+
+void producer(int id) {
+	std::unique_lock<std::mutex> lck(mtx);
+	/*while (cargo != 0)
+	{
+		std::cout << "12111111111" << std::endl;
+		produce.wait(lck);
+	}*/
+		
+	cargo = id;
+	consume.notify_one();
+}
 
 int main()
 {
+#if 0
+	//notify_all
+	std::thread t[10];
+	for (int i = 0; i < 10; ++i)
+		t[i] = std::thread(print_int,i);
+	go();
+	for (int i = 0; i < 10; ++i)
+		t[i].join();
+
+
+#endif
+#if 1
+	//重点中的重点：wait函数调用的时候，会阻塞线程，并且接触lck,如果通知接触wait阻塞的话，那么lck会锁定
+	//notify_one
+	std::thread consumers[10], producers[10];
+	// spawn 10 consumers and 10 producers:
+	for (int i = 0; i < 10; ++i) {
+		consumers[i] = std::thread(consumer);
+		producers[i] = std::thread(producer, i + 1);
+	}
+
+	// join them back:
+	for (int i = 0; i < 10; ++i) {
+		producers[i].join();
+		consumers[i].join();
+	}
+#endif
     std::cout << "Hello World!\n";
+	std::cin.get();
+	return 0;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
